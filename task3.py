721aff5c1f64267885efdde7ef74ffdbf4254c25
turduhodzhaev99 <turduhodzhaev99@gmail.com>
2020-07-17 11:57:09 +0600

    first commit

---
+++ task3.py
@@ -0,0 +1,4 @@
+guests = ["Will Smith", "Ben Aflick", "Anjelina Jolie"]
+print("Deear "  + (guests[0]),  "can you accept my invite for dinner")
+print("Deear "  + (guests[1]),  "can you accept my invite for dinner")
+print("Deear "  + (guests[2]),  "can you accept my invite for dinner")
